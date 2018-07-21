61dfaffd45dd8c7c50af26e6fe5740ae314949a9
kttalley <talleykristian@gmail.com>
2018-07-20 18:55:17 -0700

    added a few different template variables and user objects

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,12 +1,119 @@
-"use strict";
+'use strict';
 
 console.log('app.js is running!');
 
-var template = React.createElement(
-    "h1",
+//babel src/app.js --out-file=public/scripts/app.js --presets=env,react --watch
+
+
+//create app object title/subtitle
+//use title/subtitle in template
+//render template.
+
+
+var rootApp = {
+    title: 'Indecision App',
+    subTitle: 'Put yo life in the hands of a computah'
+};
+
+var indecisonTemplate = React.createElement(
+    'div',
     null,
-    " Indecision App"
+    React.createElement(
+        'h1',
+        null,
+        ' ',
+        rootApp.title,
+        ' '
+    ),
+    React.createElement(
+        'h3',
+        null,
+        ' ',
+        rootApp.subTitle,
+        ' '
+    )
 );
+
+var template = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        ' Indecision App'
+    ),
+    React.createElement(
+        'h2',
+        null,
+        ' Kristian '
+    ),
+    React.createElement(
+        'p',
+        null,
+        ' This is some info.'
+    ),
+    React.createElement(
+        'ol',
+        null,
+        React.createElement(
+            'li',
+            null,
+            'Line item one.'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'Line item two.e'
+        )
+    )
+);
+
+// Create a templateTwo var jsx expression
+// div
+//  h1 -> Kristian Talley
+//  p -> age: 26
+//  p -> location:
+// render templateTwo instead of template
+
+var userName = 'KTHIZZLE';
+var userAge = 303;
+var counter = 0;
+var userLocation = 'urButt';
+
+var user = {
+    name: 'KTT',
+    age: 33,
+    location: 'beb'
+};
+
+for (var x = 0; x < 1000; x++) {
+    counter += x;
+    console.log(counter); //lol
+}
+
+var templateTwo = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        user.name //unable to render entire objects, only their props.     
+
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Age: ',
+        userAge
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Location: ',
+        userLocation
+    )
+);
+
 var appRoot = document.getElementById("app");
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(indecisonTemplate, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,9 +1,92 @@
 console.log('app.js is running!');
 
-var template = <h1> Indecision App</h1>
-var appRoot = document.getElementById("app")
+//babel src/app.js --out-file=public/scripts/app.js --presets=env,react --watch
+
+
+
+
+
+//create app object title/subtitle
+//use title/subtitle in template
+//render template.
+
+
+var rootApp = {
+    title: 'Indecision App',
+    subTitle: 'Put yo life in the hands of a computah'
+}
+
+var indecisonTemplate = (
+    <div>
+        <h1> {rootApp.title} </h1>
+        <h3> {rootApp.subTitle} </h3>
+    </div>
+);
+
+
+var template = ( 
+    <div> 
+        <h1> Indecision App</h1>
+        <h2> Kristian </h2>
+        <p> This is some info.</p>
+        <ol>
+            <li>
+                Line item one.
+            </li>
+            <li>
+                Line item two.e
+            </li>
+        </ol>
+    </div>
+);
+
+// Create a templateTwo var jsx expression
+// div
+//  h1 -> Kristian Talley
+//  p -> age: 26
+//  p -> location:
+// render templateTwo instead of template
+
+let userName = 'KTHIZZLE';
+let userAge = 303;
+let counter = 0;
+let userLocation = 'urButt';
+
+let user = {
+    name: 'KTT',
+    age: 33,
+    location: 'beb'
+}
+
+for ( var x = 0; x < 1000; x++){
+    counter+=x;
+    console.log(counter); //lol
+}
+
+let templateTwo = (
+    <div>
+        <h1>{
+            user.name    //unable to render entire objects, only their props.     
+        }</h1>
+        <p> 
+            Age: {userAge}
+        </p>
+        <p> 
+            Location: {userLocation}
+        </p>
+
+    </div>
+);
+
+
+
+
+
+
+
+var appRoot = document.getElementById("app");
 
 ReactDOM.render(
-    template,
+    indecisonTemplate,
     appRoot
 );
