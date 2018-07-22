9e8964bb43dfb15f9d0e7acf75ae6201d7ac1244
kttalley <talleykristian@gmail.com>
2018-07-21 18:23:13 -0700

    more fun with arrow functions and methods.. forEach vs map method notes included in playground directory

--- src/playground/es6-arrow-functions.js
+++ src/playground/es6-arrow-functions.js
@@ -1,15 +1,51 @@
-const square = function (x) {
-    return x*x;
-};
+// const square = function (x) {
+//     return x*x;
+// };
 
 // const squareArrow = (x) => {
 //     return x * x;
 // };
 
-const squareArrow = (x) => x * x; 
+// const squareArrow = (x) => x * x; 
 
 
 
-console.log(square(8));
-console.log(squareArrow(9));
+// console.log(square(8));
+// console.log(squareArrow(9));
 //6:34
+
+
+//challenge getFirstName('Mike Stench') -> "Mike"
+//create regular arrow function
+//create arrow function using shorthand syntax
+
+const fullName1 = "Artemis Fowl";
+const fullName2 = "Bart Simpson";
+let firstName;
+
+const getFirstName = (name) => {
+     firstName = name.split(' ')[0];
+    console.log(firstName);
+}
+getFirstName(fullName1);
+
+//shortened arrow functions without curly brackets
+//can only have one expression and be used only
+//to return one value
+
+//versus logging something to console.
+
+let getFirstNameShortened = (name) => name.split(' ')[0];
+console.log(getFirstNameShortened(fullName2));
+
+//long hand arrow, side effect functions
+let lastName;
+const getLastName = (name) => {
+    lastName = name.split(' ')[1];
+    console.log('    ' + lastName);
+}
+getLastName(fullName2);
+
+const getLastNameShortened = (name) => name.split(' ')[1];
+
+console.log( getLastNameShortened(fullName1) );
--- src/playground/es6-arrow-functionsPt2.js
+++ src/playground/es6-arrow-functionsPt2.js
@@ -1,31 +1,25 @@
-'use strict';
-
 //arguments object - no longer bound with arrow functions
 
-var add = function add(a, b) {
+const add = function (a, b) {
     console.log(arguments);
     //the arguments object, which is an array of all inputs given, 
     //is not accessible in arrow functions
-    return a + b;
+    return a+b;
 };
 
-var sub = function sub(a, b) {
-    return a - b;
-};
+const sub = (a, b) => {
+    return a-b;
+}
 
-console.log(add(56, 8));
-console.log(sub(56, 8));
+console.log(add(56,8));
+console.log(sub(56,8));
 
 //anon functions vs declared
-var user = {
+const user = {
     name: 'Kristian',
     cities: ['Santa Cruz', 'Moses Lake', 'Ankara'],
-    printPlacesLived: function printPlacesLived() {
-        var _this = this;
-
-        return this.cities.map(function (city) {
-            return _this.name + ' has lived in ' + city;
-        });
+    printPlacesLived() {
+        return this.cities.map( (city) => this.name + ' has lived in ' + city);
         // return cityMessages;
         // console.log(this.name);
         // console.log(this.cities);
@@ -35,25 +29,22 @@ var user = {
         //     console.log(this.name + ' has lived in ' + city);
         // });
     }
+
 };
 
-console.log(user.printPlacesLived());
+console.log (user.printPlacesLived() );
 
 //challenge
 
-var multiplier = {
+const multiplier = {
     //numbers - array of numbers
     //multiplyBy - single number
     //multiply - return a new array of resulting values
 
-    numbers: [10, 20, 30],
+    numbers: [10,20,30],
     multiplyBy: 3,
-    multiply: function multiply() {
-        var _this2 = this;
-
-        return this.numbers.map(function (number) {
-            return number * _this2.multiplyBy;
-        });
+    multiply() {
+         return this.numbers.map( (number) => number * this.multiplyBy); 
     }
 };
 
@@ -61,6 +52,7 @@ var multiplier = {
 //forEach returns nothing.
 
 //map returns the modified elements
+
 
 
 console.log(multiplier.multiply());
