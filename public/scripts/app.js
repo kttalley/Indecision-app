b90579d05b3744cfabd753c32c723fdc14b0ae48
kttalley <talleykristian@gmail.com>
2018-07-27 20:56:13 -0700

    recreating app using react component classes and subclasses

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,134 +1,76 @@
 'use strict';
 
-var _get = function get(object, property, receiver) { if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { return get(parent, property, receiver); } } else if ("value" in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } };
-
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+var Header = function (_React$Component) {
+    _inherits(Header, _React$Component);
 
-var Person = function () {
-    function Person() {
-        var name = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'Anonymous';
-        var age = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 0;
+    function Header() {
+        _classCallCheck(this, Header);
 
-        _classCallCheck(this, Person);
-
-        //before function defaults, this is how you would avoid undefined,
-        this.name = name || 'test'; //function default overrides || operator, which would otherwise render test else undefined. 
-        this.age = age;
-    } //no comma after constructor method
-
-
-    _createClass(Person, [{
-        key: 'getGreeting',
-        value: function getGreeting() {
-            //return 'Yo, I am ' + this.name + '!';
-            return 'Hi, I am a ' + this.name; //${} to inject expressions into strings
-            //es6 string with backtick, next to 1 key
-        }
-    }, {
-        key: 'getDescription',
-        value: function getDescription() {
-            return this.name + ' is ' + this.age + ' year(s) old.';
-        }
-    }]);
-
-    return Person;
-}();
-//es6 subclass syntax example
-//inherits all the functionality of Person, then some
-
-
-var Student = function (_Person) {
-    _inherits(Student, _Person);
-
-    function Student(name, age) {
-        var major = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : 'Undecided';
-
-        _classCallCheck(this, Student);
-
-        //exactly the same as calling parent constructor function
-        var _this = _possibleConstructorReturn(this, (Student.__proto__ || Object.getPrototypeOf(Student)).call(this, name, age));
-
-        _this.major = major;
-        //only need to bind the extended constructor argument
-        //IF the initial parent constructor of non-subclass is evaluated
-        return _this;
-    }
-    //methods extend from Person class
-
-
-    _createClass(Student, [{
-        key: 'hasMajor',
-        value: function hasMajor() {
-            return !!this.major;
-        }
-        //empty strings are falsey--fails if test
-        //however !'' is true
-        //!! '' false
-        //!! 'Kristian Talley' True
-
-    }, {
-        key: 'getDescription',
-        value: function getDescription() {
-            var description = _get(Student.prototype.__proto__ || Object.getPrototypeOf(Student.prototype), 'getDescription', this).call(this);
-            //change/override behavior of parent method
-            if (this.hasMajor()) {
-                description += ' their major is ' + this.major;
-            }
-            return description;
-        }
-    }]);
-
-    return Student;
-}(Person);
-
-//override getGreeting
-//1. Hi I am {name}
-//2. I'm visiting from {homeLocation}
-
-
-var Traveler = function (_Person2) {
-    _inherits(Traveler, _Person2);
-
-    function Traveler(name, age, homeLocation) {
-        _classCallCheck(this, Traveler);
-
-        var _this2 = _possibleConstructorReturn(this, (Traveler.__proto__ || Object.getPrototypeOf(Traveler)).call(this, name, age, homeLocation));
-
-        _this2.homeLocation = homeLocation;
-        return _this2;
+        return _possibleConstructorReturn(this, (Header.__proto__ || Object.getPrototypeOf(Header)).apply(this, arguments));
     }
 
-    _createClass(Traveler, [{
-        key: 'getGreeting',
-        value: function getGreeting() {
-            var greeting = _get(Traveler.prototype.__proto__ || Object.getPrototypeOf(Traveler.prototype), 'getGreeting', this).call(this);
-            if (this.homeLocation) {
-                greeting += ' I am visiting from ' + this.homeLocation;
-            }
-            return greeting;
+    _createClass(Header, [{
+        key: 'render',
+        value: function render() {
+            return React.createElement(
+                'div',
+                null,
+                React.createElement(
+                    'h1',
+                    null,
+                    'Indecision'
+                ),
+                React.createElement(
+                    'h2',
+                    null,
+                    'App'
+                )
+            );
         }
     }]);
 
-    return Traveler;
-}(Person);
+    return Header;
+}(React.Component);
 
-var me = new Student('Kristian Talley', 26, 'Art');
-console.log(me.getDescription());
+var Action = function (_React$Component2) {
+    _inherits(Action, _React$Component2);
 
-var other = new Student();
-console.log(other.getDescription());
+    function Action() {
+        _classCallCheck(this, Action);
 
-var vlad = new Traveler('Vlad', '55', 'Russia');
-console.log(vlad.getGreeting());
+        return _possibleConstructorReturn(this, (Action.__proto__ || Object.getPrototypeOf(Action)).apply(this, arguments));
+    }
 
-var drumpf = new Traveler('Donold', '69', 'Queens');
-console.log(drumpf.getGreeting());
+    _createClass(Action, [{
+        key: 'render',
+        value: function render() {
+            return React.createElement(
+                'div',
+                null,
+                React.createElement(
+                    'button',
+                    null,
+                    ' What should I do next? '
+                )
+            );
+        }
+    }]);
 
-var homeward = new Traveler('Ralf');
-console.log(homeward.getGreeting());
+    return Action;
+}(React.Component);
+
+var templateContainer = React.createElement(
+    'div',
+    null,
+    React.createElement(Header, null),
+    React.createElement(Action, null)
+);
+ReactDOM.render(templateContainer, document.getElementById('app'));
--- src/app.js
+++ src/app.js
@@ -1,110 +1,31 @@
-console.log('app.js is running!');
-
-//babel src/app.js --out-file=public/scripts/app.js --presets=env,react --watch
-
-
-
-
-
-//create app object title/subtitle
-//use title/subtitle in template
-//render template.
-
-
-//only render the subtitle (and p tag) if subtitle exists -- logical & operator
-//render new p tag - if options.length > 0 "here are your options"
-//if not, no options.
-
-
-const rootApp = {
-    title: 'Indecision App',
-    subTitle: 'Put yo life in the hands of a computah',
-    options: []
-
+class Header extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Indecision</h1>
+                <h2>App</h2>
+            </div>
+        )
+    }
+}
+class Action extends React.Component {
+    render() {
+        return (
+            <div>
+                <button> What should I do next? </button>
+            </div>
+        );
+    }
 }
 
-let optionCount = 0;
+const templateContainer = (
+    <div>
+        <Header />
+        <Action />
 
-const onFormSubmit = (e) => {
-    e.preventDefault();
-    const option = e.target.elements.option.value;
-    
-    if (option) {
-        rootApp.options.push(option);
-        e.target.elements.option.value = '';
-        // optionCount++;
-        optionCount = rootApp.options.length;
-    
-    }
-    reRenderTemplate();
-};
-
-const subButton = () => {
-    rootApp.options.length > 0 && rootApp.options.splice(-1);
-    optionCount = rootApp.options.length;
-    reRenderTemplate();
-};
-
-const resetButton = () => {
-    rootApp.options = [];
-    optionCount = rootApp.options.length;
-    reRenderTemplate();
-    
-};
-//floor method rounds, random method chooses a number between 0 and 0.999
-const onMakeDecision = () => {
-    const randomNum = Math.floor(Math.random() * rootApp.options.length);
-    const option = rootApp.options[randomNum];
-    alert(option);
-    // console.log(randomNum);
-};
-
-
-
-const numbers = [200, 55, 101];
-
-const reRenderTemplate = () => {
-    const indecisonTemplate = (
-        <div>
-            <h1> {rootApp.title} </h1>
-            { rootApp.subTitle && <h3>{rootApp.subTitle}</h3>}
-            {/*only render subtitle if it exists*/}
-            {rootApp.options.length > 0 ? <p> There be options. </p> : <p>No options.</p>}
-            <p> Amount of Options: {optionCount}</p>
-            <button disabled = {rootApp.options.length === 0} onClick = {onMakeDecision}> What should I do? </button>
-            <ol>
-                {/* map over app.options getting back an array of list */}
-                {rootApp.options.map( (option) => {
-                  return <li key = {option}>{option} </li>  
-                })}
-            </ol>
-    
-            <form onSubmit = {onFormSubmit}>
-                <input type ="text" name = "option"/>
-                <button>Add Option</button>
-            </form>
-            
-            <button onClick = {subButton}>
-                Remove last option.
-            </button>
-    
-            <button onClick = {resetButton}>
-                Restart your to-do list.
-            </button>
-            {
-               // [ <p key = '0'>a</p>, <p key = '1'>b</p>, <p key = '2'>c</p>]
-                numbers.map( (number) => <p key={number}>Number: {number}</p>)
-            }
-        </div>
-    );
-    ReactDOM.render(
-        indecisonTemplate,
-            appRoot
-    );
-};
-
-
-
-const appRoot = document.getElementById('app');
-
-reRenderTemplate();
+    </div>
+)
+ReactDOM.render (
+    templateContainer,
+    document.getElementById('app')
+);
