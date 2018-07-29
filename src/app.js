d9a098c9e5d25b94a3d959cc4af36bb337ccef10
kttalley <talleykristian@gmail.com>
2018-07-29 15:24:53 -0700

    dealing with bind() method in class components

--- src/app.js
+++ src/app.js
@@ -1,31 +1,127 @@
-class Header extends React.Component {
+// const obj = {
+//     name: 'Vikram',
+//     getName() {
+//         return this.name;
+//     }
+// };
+
+// const getName = obj.getName.bind(obj);
+
+
+// console.log(getName());
+
+class IndecisionApp extends React.Component{
     render() {
+        const title = 'Indecision';
+        const subTitle = 'Put your life in the hands of a computerrrr';
+        const options = ['Thing One', `Thing Two`, `Thing Three`];
         return (
             <div>
-                <h1>Indecision</h1>
-                <h2>App</h2>
-            </div>
-        )
-    }
-}
-class Action extends React.Component {
-    render() {
-        return (
-            <div>
-                <button> What should I do next? </button>
+                <Header title = {title} subTitle = {subTitle} />
+                <Action />
+                <Options options = {options} />
+                <AddOptions />
             </div>
         );
     }
 }
 
-const templateContainer = (
-    <div>
-        <Header />
-        <Action />
+// Setup options prop for Options component
+// Render the length of the array
 
-    </div>
-)
+class Header extends React.Component {
+    render() {
+        
+        return (
+            <div>
+                <h1>{this.props.title} </h1>
+                <h2>{this.props.subTitle}</h2>
+            </div>
+        )
+    }
+}
+class Action extends React.Component {
+    handlePick() {
+        alert('handlePick');
+    }
+
+    render() {
+        return (
+            <div>
+                <button onClick = {this.handlePick}> What should I do next? </button>
+            </div>
+        );
+    }
+}
+
+class Options extends React.Component {
+    constructor(props){
+        super(props);
+        this.removeAll = this.removeAll.bind(this);
+    }
+    removeAll() {
+        //this no longer points to parent
+        console.log(this.props.options);
+        //  this.props.options.splice(-1);
+        // alert('removeAll');
+
+        // optionCount = 0;
+
+    }
+    render() {
+       return (
+           <div>
+           {
+               this.props.options.map((option) => <Option key={option} optionText={option}/>)
+           }
+           <button onClick = {this.removeAll}> Start over </button>
+           </div>
+       );
+    }
+}
+
+class Option extends React.Component {
+    render() {
+        return (
+            <div>
+               Option:  {this.props.optionText}
+            </div>
+        );
+    }
+
+}
+
+
+class AddOptions extends React.Component {
+    onFormSubmit(e){
+        e.preventDefault();
+        const option = e.target.elements.option.value.trim();
+        if (option) {
+            alert('option');        
+        }
+    }
+    render () {
+        return (
+          <div>
+            <form onSubmit = {this.onFormSubmit}>
+                <input type ="text" name = "option"/>
+                <button>Add Option</button>
+            </form>
+          </div>  
+        );
+    }
+}
+
+// const templateContainer = (
+//     <div>
+//         <Header />
+//         <Action />
+//         <Options />
+//         <AddOptions />
+
+//     </div>
+// )
 ReactDOM.render (
-    templateContainer,
+   <IndecisionApp />,
     document.getElementById('app')
 );
--- src/playground/this-binding-examples.jsx
+++ src/playground/this-binding-examples.jsx
@@ -1,23 +1,22 @@
-"use strict";
-
 //even functions are objects in JS
 //with their own properties and methods
 
 var regObjFood = {
     name: "kiwi",
     category: "Fruit",
-    makeJuice: function makeJuice() {
+    makeJuice: function () {
         var message = "Making" + this.name + "juice";
         return message;
     }
+}
 
-    // because functions in javascript are objects
-    //we can attach properties and objects to it.
-    //lets take a look.
+// because functions in javascript are objects
+//we can attach properties and objects to it.
+//lets take a look.
 
-};function Food() {}
-//logic goes here
-
+function Food() {
+    //logic goes here
+}
 // Food.name = "Kiwi";
 Food.category = "Fruit";
 Food.makeJuice = function () {
@@ -26,9 +25,10 @@ Food.makeJuice = function () {
     console.log(message);
 };
 
-Function.prototype.makeJuice = function () {
+
+Function.prototype.makeJuice = function() {
     console.log("Making " + this.name + " juice");
-};
+}
 
 var myFruit = function Food() {
     this.name = "kiwi";
