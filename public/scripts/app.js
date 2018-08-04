ba74d0120bde3ce6da0ded2c1f8cf92497828335
kttalley <talleykristian@gmail.com>
2018-08-03 19:23:01 -0700

    re-created the counter app example as a component that changes state.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,35 +1,161 @@
-"use strict";
+'use strict';
 
-//even functions are objects in JS
-//with their own properties and methods
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-var regObjFood = {
-    name: "kiwi",
-    category: "Fruit",
-    makeJuice: function makeJuice() {
-        var message = "Making" + this.name + "juice";
-        return message;
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var Counter = function (_React$Component) {
+    _inherits(Counter, _React$Component);
+
+    //state is an object that allows data to change
+    //and re-render components
+    //we set the default state in the constructor.
+    function Counter(props) {
+        _classCallCheck(this, Counter);
+
+        var _this = _possibleConstructorReturn(this, (Counter.__proto__ || Object.getPrototypeOf(Counter)).call(this, props));
+
+        _this.addOne = _this.addOne.bind(_this);
+        _this.subOne = _this.subOne.bind(_this);
+        _this.resetCount = _this.resetCount.bind(_this);
+
+        //define default state of component.
+        _this.state = {
+            count: 0
+        };
+        return _this;
     }
 
-    // because functions in javascript are objects
-    //we can attach properties and objects to it.
-    //lets take a look.
+    _createClass(Counter, [{
+        key: 'addOne',
+        value: function addOne() {
+            //this.setState() is a function that requires a function that
+            //returns a new state object. Upon assigning the new state object
+            //the component re-renders.
+            this.setState(function (prevState) {
+                return {
+                    count: prevState.count + 1
+                };
+            });
+            //setState() method does not override state, only injects changed props.
+        }
+    }, {
+        key: 'resetCount',
+        value: function resetCount() {
+            this.setState(function (prevState) {
+                //return an object to update the state object and reRender the component.
+                return {
+                    count: prevState.count = 0
+                };
+            });
+        }
+    }, {
+        key: 'subOne',
+        value: function subOne() {
+            this.setState(function (prevState) {
+                return {
+                    count: prevState.count <= 0 ? prevState.count = 0 : prevState.count - 1
 
-};function Food() {}
-//logic goes here
+                };
+            });
+        }
+    }, {
+        key: 'render',
+        value: function render() {
+            return React.createElement(
+                'div',
+                null,
+                React.createElement(
+                    'h1',
+                    null,
+                    'Count:',
+                    this.state.count,
+                    ' '
+                ),
+                React.createElement(
+                    'button',
+                    { onClick: this.addOne },
+                    ' +1 '
+                ),
+                React.createElement(
+                    'button',
+                    { onClick: this.subOne },
+                    ' -1 '
+                ),
+                React.createElement(
+                    'button',
+                    { onClick: this.resetCount },
+                    ' reset '
+                )
+            );
+        }
+    }]);
 
-// Food.name = "Kiwi";
-Food.category = "Fruit";
-Food.makeJuice = function () {
-    var message = "Making " + this.name + " juice";
-    return message;
-    console.log(message);
-};
+    return Counter;
+}(React.Component);
 
-Function.prototype.makeJuice = function () {
-    console.log("Making " + this.name + " juice");
-};
+ReactDOM.render(React.createElement(Counter, null), document.getElementById('app'));
 
-var myFruit = function Food() {
-    this.name = "kiwi";
-}.makeJuice();
+// let count = 0;
+// let someId = 'javaScriptVar-forID'
+// let subId = 'subId';
+
+// //modified data does not automatically re-render the template.
+// //Therefore having the template nested within a function
+// //that is called everytime a piece of data changes is the solution.
+
+// const addOne = () => {
+//     count++;
+//     // console.log('addOne', count);
+//     renderCounterApp();
+// };
+// const subOne = () => {
+//     count > 0 && count--;
+//     // console.log('subOne');
+
+//     renderCounterApp();
+// };
+// const resetCount = () => {
+//     count = 0;
+//     renderCounterApp();
+// };
+
+
+// //challenge make button = -1 = setup minusOne function adn register -log "minusOne"
+// //make reset button 'reset' - setup reset function - log 'reset' 
+
+
+// const appRoot = document.getElementById("app");
+
+
+// const renderCounterApp = () => {
+//     const templateThree = (
+//         <div>
+//           <h1>
+//               Count: {count}
+//           </h1>
+//           <button id = {someId} className = "button" onClick = { addOne }>
+//               +1
+//           </button>
+//           <button id = {subId} className = "subButton" onClick = { subOne }>
+//               -1
+//           </button>
+//           <button id = {someId} className = "button" onClick = { resetCount }>
+//               Reset
+//           </button>
+//         </div>
+//       );
+
+//       ReactDOM.render(
+//         // indecisonTemplate,
+//         // templateTwo,
+//         templateThree,
+//         appRoot
+//     );
+// };
+
+// renderCounterApp();
--- src/playground/counter-app.js
+++ src/playground/counter-app.js
@@ -1,65 +1,136 @@
-let count = 0;
-let someId = 'javaScriptVar-forID'
-let subId = 'subId';
-
-//modified data does not automatically re-render the template.
-//Therefore having the template nested within a function
-//that is called everytime a piece of data changes is the solution.
-
-const addOne = () => {
-    count++;
-    // console.log('addOne', count);
-    renderCounterApp();
-};
-const subOne = () => {
-    count > 0 && count--;
-    // console.log('subOne');
+class Counter extends React.Component {
     
-    renderCounterApp();
-};
-const resetCount = () => {
-    count = 0;
-    renderCounterApp();
-};
+    //state is an object that allows data to change
+    //and re-render components
+    //we set the default state in the constructor.
+    constructor(props){
+        super(props);
+        this.addOne = this.addOne.bind(this);
+        this.subOne = this.subOne.bind(this);
+        this.resetCount = this.resetCount.bind(this);
+
+        //define default state of component.
+        this.state = {
+            count: 0
+        };
+    }
+    addOne(){
+        //this.setState() is a function that requires a function that
+        //returns a new state object. Upon assigning the new state object
+        //the component re-renders.
+        this.setState( (prevState) => {
+            return {
+                count: prevState.count + 1
+            }
+        });
+        //setState() method does not override state, only injects changed props.
+    }
+
+    resetCount(){
+       this.setState( (prevState) => {
+           //return an object to update the state object and reRender the component.
+            return {
+                count: prevState.count = 0
+            }
+       });
+
+
+    }
+
+    subOne(){
+        this.setState( (prevState) => {
+            return {
+                count: 
+                prevState.count <= 0 ? prevState.count = 0 : prevState.count -1
+               
+            }
+        });
+    }
+    render() {
+        return (
+            <div>
+                <h1>Count:{this.state.count} </h1>
+                <button onClick = {this.addOne}> +1 </button>
+                <button onClick = {this.subOne}> -1 </button>
+                <button onClick = {this.resetCount}> reset </button>
+            </div>
+        );
+    }
+
+}
+
+ReactDOM.render(
+    <Counter />,
+    document.getElementById('app')
+);
 
 
 
-//challenge make button = -1 = setup minusOne function adn register -log "minusOne"
-//make reset button 'reset' - setup reset function - log 'reset' 
+
+
+
+// let count = 0;
+// let someId = 'javaScriptVar-forID'
+// let subId = 'subId';
+
+// //modified data does not automatically re-render the template.
+// //Therefore having the template nested within a function
+// //that is called everytime a piece of data changes is the solution.
+
+// const addOne = () => {
+//     count++;
+//     // console.log('addOne', count);
+//     renderCounterApp();
+// };
+// const subOne = () => {
+//     count > 0 && count--;
+//     // console.log('subOne');
+    
+//     renderCounterApp();
+// };
+// const resetCount = () => {
+//     count = 0;
+//     renderCounterApp();
+// };
+
+
+
+// //challenge make button = -1 = setup minusOne function adn register -log "minusOne"
+// //make reset button 'reset' - setup reset function - log 'reset' 
 
 
 
 
 
 
-const appRoot = document.getElementById("app");
+// const appRoot = document.getElementById("app");
 
 
 
-const renderCounterApp = () => {
-    const templateThree = (
-        <div>
-          <h1>
-              Count: {count}
-          </h1>
-          <button id = {someId} className = "button" onClick = { addOne }>
-              +1
-          </button>
-          <button id = {subId} className = "subButton" onClick = { subOne }>
-              -1
-          </button>
-          <button id = {someId} className = "button" onClick = { resetCount }>
-              Reset
-          </button>
-        </div>
-      );
+// const renderCounterApp = () => {
+//     const templateThree = (
+//         <div>
+//           <h1>
+//               Count: {count}
+//           </h1>
+//           <button id = {someId} className = "button" onClick = { addOne }>
+//               +1
+//           </button>
+//           <button id = {subId} className = "subButton" onClick = { subOne }>
+//               -1
+//           </button>
+//           <button id = {someId} className = "button" onClick = { resetCount }>
+//               Reset
+//           </button>
+//         </div>
+//       );
 
-      ReactDOM.render(
-        // indecisonTemplate,
-        // templateTwo,
-        templateThree,
-        appRoot
-    );
-};
+//       ReactDOM.render(
+//         // indecisonTemplate,
+//         // templateTwo,
+//         templateThree,
+//         appRoot
+//     );
+// };
 
-renderCounterApp();
+// renderCounterApp();
